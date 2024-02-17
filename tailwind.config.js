c7152154082a7e37284dc779f2719aec66439ecb
brownlock2203 <brandonmedehou2203@gmail.com>
2024-02-17 17:50:25 +0100

    IOS15 with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    content: ['./src/**/*.html', './src/**/*.js'],
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
